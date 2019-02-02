551acfd0e9bce80ca04d76d5c0674b379790fdb2
EC2 Default User <ec2-user@ip-172-31-12-207.us-east-2.compute.internal>
2019-02-02 16:16:01 +0900

    controllerの記述を修正。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'economic_indicator/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'index' => 'economic_indicator#index'
+
 end
